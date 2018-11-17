6413375a3694aedcf875d2001993abac619ddf89
Wayne Lee <waynelee1000@gmail.com>
2018-11-17 16:58:26 -0500

    Created Preference and Friend Objects

---
+++ TermProject/Classes/Friend.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Classes
+{
+    public class Friend
+    {
+        public string LoginID { get; set; }
+        public string FriendLoginID { get; set; }
+
+        public Friend()
+        {
+
+        }
+        public Friend(string loginID, string friendLoginID)
+        {
+            this.LoginID = loginID;
+            this.FriendLoginID = friendLoginID;
+        }
+
+        public List<Friend> Friendlist(string loginID)
+        {
+            StoredProcedure storedProcedure = new StoredProcedure();
+            DataSet friendData = new DataSet();
+            List<Friend> friendList = new List<Friend>();
+            friendData = storedProcedure.getFriend(loginID);
+
+            if (friendData.Tables[0].Rows.Count != 0)
+            {
+                foreach (int row in friendData.Tables[0].Rows)
+                {
+                    Friend friend = new Friend(loginID, friendData.Tables[0].Rows[row][0].ToString());
+                    friendList.Add(friend);
+                }
+            }
+
+            return friendList;
+
+        }
+    }
+}
