8df3cdb59621cc429ced6ded82eda6ce3a58e260
Wayne Lee <waynelee1000@gmail.com>
2018-12-10 14:59:48 -0500

    Revert "Merge branch 'master' of https://github.com/waynelee1000/TermProject3342"
    This reverts commit f9f54212c10ab1fb391df20b3892258d25250535, reversing
    changes made to b2b2712d5e8d12cc4d34f576df27ecd8fb840139.

--- TermProject/TermProject/Main Pages/messages.aspx.cs
+++ TermProject/TermProject/Main Pages/messages.aspx.cs
@@ -18,132 +18,49 @@ namespace TermProject.Main_Pages
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                // Get user's name
-                Encrypt encrypt = new Encrypt();
-                HttpCookie userCookie = Request.Cookies["UserCookie"];
-                string UserName = userCookie.Values["Name"].ToString();
-                string LoginID = encrypt.Decrypt(userCookie.Values["Username"].ToString());
+            // Get user's name
+            Encrypt encrypt = new Encrypt();
+            HttpCookie userCookie = Request.Cookies["UserCookie"];
+            string UserName = userCookie.Values["Name"].ToString();
+            string LoginID = userCookie.Values["Username"].ToString();
 
-                // Get thread data from database
-                StoredProcedure storedProcedure = new StoredProcedure();
-                DataSet threadsData = new DataSet();
-                threadsData = storedProcedure.GetThreads(UserName);
+            // Get thread data from database
+            StoredProcedure storedProcedure = new StoredProcedure();
+            DataSet threadsData = new DataSet();
+            threadsData = storedProcedure.GetThreads(UserName);
 
-                // Bind thread data to DDL
-                threadsDDL.DataSource = threadsData.Tables[0];
-                threadsDDL.DataTextField = "ThreadID";
-                threadsDDL.DataValueField = "ThreadID";
-                threadsDDL.DataBind();
+            // Bind thread data to DDL
+            threadsDDL.DataSource = threadsData.Tables[0];
+            threadsDDL.DataTextField = "ThreadID";
+            threadsDDL.DataValueField = "ThreadID";
+            threadsDDL.DataBind();
 
-                // Get users friend array for new thread DDL
-                Friend theFriend = new Friend();
-                Friend[] myFriends = theFriend.FriendArray(LoginID);
-                String[] friendArray = theFriend.GetNameArray(myFriends);
+            // Get users friend array for new thread DDL
+            Friend theFriend = new Friend();
+            Friend[] myFriends = theFriend.FriendArray(LoginID);
 
-                // Bind friend data to ddl
-                newThreadCBL.DataSource = friendArray;
-                newThreadCBL.DataBind();
+            // Bind friend data to ddl
+            newThreadDDL.DataSource = myFriends;
+            newThreadDDL.DataBind();
 
-                // Hide elements until button is pressed
-                newThreadCBL.Style["visibility"] = "hidden";
-                newThreadBtn.Style["visibility"] = "hidden";
-                showFriendsBtn.Style["visibility"] = "visible";
+            NavButtons nav = (NavButtons)LoadControl("NavButtons.ascx");
+            headerButtons.Controls.Add(nav);
 
-                //
-                string threadID = threadsDDL.SelectedValue;
-                DataSet myDS = new DataSet();
-                myDS = storedProcedure.GetMessages(threadID);
-                threadGrid.DataSource = myDS;
-                threadGrid.DataBind();
-            }
-        }
-        protected void logoutBtn_Click(object sender, EventArgs e)
-        {
-            Session["LoginStatus"] = "False";
-
-            Response.Redirect("~/Login Pages/login.aspx");
-        }
-
-        protected void prefBtn_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("~/Main Pages/preferences.aspx");
-        }
-
-        protected void messagesBtn_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("~/Main Pages/messages.aspx");
-        }
-
-        protected void profileBtn_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("~/Main Pages/profile.aspx");
         }
 
         protected void newThreadBtn_Click(object sender, EventArgs e)
         {
-            List<String> selected = new List<String>();
-            foreach (ListItem item in newThreadCBL.Items)
-            {
-                if (item.Selected)
-                {
-                    selected.Add(item.Value);
-                }
-            }
-            if (selected.Count == 0)
-            {
-                return;
-            } else
-            {
-                string newThreadTitle = "";
-                foreach (string item in selected)
-                {
-                    newThreadTitle += (item + ", ");                   
-                }
-                StoredProcedure storedProcedure = new StoredProcedure();
-                HttpCookie userCookie = Request.Cookies["UserCookie"];
-                string messageText = "New Thread Created";
-                string userName = userCookie.Values["Name"];
-                storedProcedure.SendMessage(messageText, newThreadTitle, userName);
-                Response.Redirect("~/Main Pages/messages.aspx");
-            }
 
         }
 
         protected void threadGrid_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
-            
+
         }
 
         protected void threadsDDL_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string threadID = threadsDDL.SelectedValue;
-            StoredProcedure storedProcedure = new StoredProcedure();
-            DataSet myDS = new DataSet();
-            myDS = storedProcedure.GetMessages(threadID);
-            threadGrid.DataSource = myDS;
-            threadGrid.DataBind();
-        }
 
-        protected void showFriendsBtn_Click(object sender, EventArgs e)
-        {
-            newThreadCBL.Style["visibility"] = "visible";
-            showFriendsBtn.Style["visibility"] = "hidden";
-            newThreadBtn.Style["visibility"] = "visible";
-        }
-
-        protected void messageBtn_Click(object sender, EventArgs e)
-        {
-            HttpCookie userCookie = Request.Cookies["UserCookie"];
-            string messageText = messageTxt.Text;
-            string threadID = threadsDDL.SelectedValue;
-            string userName = userCookie.Values["Name"];
-
-            StoredProcedure storedProcedure = new StoredProcedure();
-
-            storedProcedure.SendMessage(messageText, threadID, userName);
-            Response.Redirect("~/Main Pages/messages.aspx");
         }
     }
 }
--- TermProject/TermProject/Main Pages/newsfeed.aspx.cs
+++ TermProject/TermProject/Main Pages/newsfeed.aspx.cs
@@ -92,8 +92,8 @@ namespace TermProject.Main_Pages
                 UpdatePanel1.ContentTemplateContainer.Controls.Add(new LiteralControl("<br />"));
                 Label owner = new Label();
 
-                owner.Text = x.Name;
-                UpdatePanel1.ContentTemplateContainer.Controls.Add(owner);
+                owner.Text = x.L;
+                UpdatePanel1.ContentTemplateContainer.Controls.Add(label);
 
                 UpdatePanel1.ContentTemplateContainer.Controls.Add(new LiteralControl("<br />"));
                 Label label = new Label();
